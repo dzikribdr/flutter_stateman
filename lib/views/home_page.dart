4c2b0b43cca3e8f595e38721fab14c91ad4c0f8c
dzikriabdr <dzikriabdr@gmail.com>
2026-02-27 02:35:55 +0700

    ganti builder jadi consumer

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/counter_model.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Counter App")),
-      body: Center(
-        child: Text('$_counter', style: const TextStyle(fontSize: 40)),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        child: const Icon(Icons.add),
-      ),
+    return Consumer<CounterModel>(
+      builder: (context, counter, _) {
+        return Scaffold(
+          appBar: AppBar(title: const Text("Counter App")),
+          body: Center(
+            child: Text(
+              '${counter.counter}',
+              style: const TextStyle(fontSize: 40),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => counter.increment(),
+            child: const Icon(Icons.add),
+          ),
+        );
+      },
     );
   }
 }
