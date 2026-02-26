701a15aaba3b9c9a698c02c5d2d631711ebfd019
dzikriabdr <dzikriabdr@gmail.com>
2026-02-27 02:34:29 +0700

    update counter model pake changenotifier

--- lib/models/counter_model.dart
+++ lib/models/counter_model.dart
@@ -1,1 +1,10 @@
-class CounterModel {}
+import 'package:flutter/material.dart';
+
+class CounterModel extends ChangeNotifier {
+  int counter = 0;
+
+  void increment() {
+    counter++;
+    notifyListeners();
+  }
+}
