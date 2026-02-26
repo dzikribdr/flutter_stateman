346a8cba7e2adbe8fc6e23b45ba883b9cf1c6236
dzikriabdr <dzikriabdr@gmail.com>
2026-02-27 02:31:53 +0700

    menambahkan code untuk homepage

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() => _counter++);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Counter App")),
+      body: Center(
+        child: Text('$_counter', style: const TextStyle(fontSize: 40)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
